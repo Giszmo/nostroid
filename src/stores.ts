e0a072155445d2569e49ddeb950ff882e73bead6
userquin <userquin@gmail.com>
2022-09-09 18:35:33 +0200

    chore: fix some types and missing entries

--- src/stores.ts
+++ src/stores.ts
@@ -1,45 +1,46 @@
-import { db } from "./db"
-import type { IProfile } from "./db"
-import { liveQuery } from "dexie"
-import { writable } from 'svelte/store'
+import { db } from './db';
+import type { IProfile } from './db';
+import { liveQuery } from 'dexie';
+import { writable } from 'svelte/store';
 
-export const activeProfile = liveQuery(async (): IProfile|undefined => {
-  const pubkey = (await db.config.get('activePubkey'))?.value
-  if (pubkey) {
-    const profile = await db.profiles.get(pubkey)
-    if (profile) {
-      return profile
-    }
-  }
-  return {name: '', pubkey: ''}
-})
+export const activeProfile = liveQuery(async (): Promise<IProfile | undefined> => {
+	const pubkey = (await db.config.get('activePubkey'))?.value;
+	if (pubkey) {
+		const profile = await db.profiles.get(pubkey);
+		if (profile) {
+			return profile;
+		}
+	}
+	return { name: '', pubkey: '', isAccount: false };
+});
 
 /**
  * A Map but whenever a value is missing, an attempt is made to retrieve it.
  **/
 export class ProfileCache {
-  public backing = new Map<string, IProfile>()
+	public backing = new Map<string, IProfile>();
 
-  public get(pubkey: string): IProfile {
-    let profile = this.backing.get(pubkey)
-    if (profile == undefined) {
-      profile = {
-        pubkey: pubkey,
-        missing: true
-      } as IProfile
-      db.profiles.put(profile)
-      this.backing.set(pubkey, profile)
-    }
-    return profile
-  }
-  
-  public set(s: string, p: IProfile) {
-    return this.backing.set(s, p)
-  }
+	public get(pubkey: string): IProfile {
+		let profile = this.backing.get(pubkey);
+		if (profile == undefined) {
+			profile = {
+				pubkey: pubkey,
+				missing: true,
+				isAccount: false
+			} as IProfile;
+			db.profiles.put(profile);
+			this.backing.set(pubkey, profile);
+		}
+		return profile;
+	}
+
+	public set(s: string, p: IProfile) {
+		return this.backing.set(s, p);
+	}
 }
 
 /**
  * cProfiles stands for cached profiles
  * if we have a profile, cProfiles[pubkey] should hold it
  **/
-export const cProfiles = writable(new ProfileCache())
+export const cProfiles = writable(new ProfileCache());
