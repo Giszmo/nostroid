014459857bab27572979f1301f4b14fa40e0860e
Vishal <64505169+vishalxl@users.noreply.github.com>
2022-09-24 21:35:36 +0530

    defined global.process in vite config file
    Before this, npm run build was giving error when validateEvent was run which internally used global.process, which came as undefined ( and no events were fetched) .
    
    Now it runs, and fetches events.

--- vite.config.ts
+++ vite.config.ts
@@ -39,6 +39,8 @@ export default defineConfig({
 	},
 	build: {
 		sourcemap: 'inline' // helpful for debugging, maybe remove in production
-	},
-  global: {}
+	}, 
+	define: { 
+		'global.process':  JSON.stringify('window.process')
+	}
 });
