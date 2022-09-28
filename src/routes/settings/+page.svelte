b63f5d85e8a9fee6bd9b596ad01ff7bf5cd07e8d
Leo Wandersleb <leo@leowandersleb.de>
2022-09-28 15:29:52 -0300

    fix settings trying to store under wrong privkey

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -8,17 +8,11 @@
   let pk = ''
   let nip05 = ''
   
-  $: {(async () => {
-    if ($activeProfile && $activeProfile?.pubkey && $activeProfile?.pubkey != pk) {
-      pk = $activeProfile.pubkey
-      name = $activeProfile.name || ''
-      avatar = $activeProfile.avatar || ''
-      nip05 = $activeProfile.nip05 || ''
-    } else {
-      cancelPersist()
-    }
-  })()
-}
+  let outEvent: {privkey: string, content: string}|undefined
+  
+  $: { if ($activeProfile && $activeProfile?.pubkey != pk) {
+    reset()
+  }}
 
 var interval: any // NodeJs.Timer? number?
 let delayS = 10
@@ -44,36 +38,39 @@ const cancelPersist = () => {
 
 const reset = async () => {
   cancelPersist()
-  let active = (await $activeProfile) as IProfile
-  name = active.name || ''
-  avatar = active.avatar || ''
-  nip05 = active.nip05 || ''
+  let active = $activeProfile as IProfile
+  name = active?.name || ''
+  avatar = active?.avatar || ''
+  nip05 = active?.nip05 || ''
 }
 
 const sendPersist = async () => {
-  const p = $activeProfile as IProfile
-  sendPersistEvent(0, [], JSON.stringify(
-      {
-        name: name,
-        picture: avatar,
-        nip05: nip05
-      }
-    ), p.privkey)
+  if (outEvent) {
+    sendPersistEvent(0, [], outEvent.content, outEvent.privkey)
+  }
   cancelPersist()
 }
 
 $: {
-  name
-  avatar
-  nip05
-  let a = ($activeProfile || {pubkey:''}) as IProfile
-  if (name === a.name && avatar === a.avatar && nip05 === a.nip05) {
+  let a = $activeProfile as IProfile
+  if (a?.privkey === undefined || (
+    name === a.name && avatar === a.avatar && nip05 === a.nip05)) {
+    outEvent = undefined
     cancelPersist()
   } else {
+    outEvent = {
+      privkey: a.privkey,
+      content: JSON.stringify(
+        {
+          name: name,
+          picture: avatar,
+          nip05: nip05
+        }
+      )
+    }
     persistLater()
   }
 }
-
 </script>
 
 <svelte:head>
@@ -88,6 +85,7 @@ $: {
       <label>PK: <input bind:value={pk} disabled></label><br>
       <label>Name: <input bind:value={name}></label><br>
       <label>Avatar: <input bind:value={avatar}></label><br>
+      <img src={avatar} alt='profile'><br>
       <label>nip05: <input bind:value={nip05}></label><br>
       {@html msg}
       <button on:click={cancelPersist}>Cancel</button>
@@ -97,6 +95,7 @@ $: {
       <strong>PK</strong>: <span>{pk}</span><br>
       <strong>Name</strong>: <span>{name}</span><br>
       <strong>Avatar</strong>: <span>{avatar}</span><br>
+      <img src={avatar} alt='profile'><br>
       <strong>nip05</strong>: <span>{nip05}</span><br>
     {/if}
   {:else}
