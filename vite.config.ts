49f1abf5b06e2a3bde34662d148102056cfdd373
snuffyDev <72365477+snuffyDev@users.noreply.github.com>
2022-10-04 19:53:04 -0400

    fixed formatting - created a .editorconfig to ensure consistency

--- vite.config.ts
+++ vite.config.ts
@@ -1,53 +1,58 @@
-import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
-import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
-import rollupNodePolyFill from "rollup-plugin-node-polyfills";
-import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig } from "vite";
-import { SvelteKitPWA } from "@vite-pwa/sveltekit";
-import { nostroidPWA } from "./nostroid-config.js";
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
+import rollupNodePolyFill from 'rollup-plugin-node-polyfills';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
+import { nostroidPWA } from './nostroid-config.js';
 
 export default defineConfig({
-	plugins: [
-		sveltekit(),
-		SvelteKitPWA(nostroidPWA),
-		NodeGlobalsPolyfillPlugin({
-			process: true,
-			buffer: true,
-		}),
-		NodeModulesPolyfillPlugin(),
-	],
-	ssr: {
-		noExternal: ["nostr-tools"],
-	},
-	resolve: {
-		alias: {
-			process: "process/browser",
-			stream: "rollup-plugin-node-polyfills/polyfills/stream",
-			util: "rollup-plugin-node-polyfills/polyfills/util",
-			// buffer: 'rollup-plugin-node-polyfills/polyfills/buffer-es6'
-		},
-	},
-	optimizeDeps: {
-		include: ["nostr-tools > create-hash", "workbox-precaching", "workbox-routing", "workbox-window"],
-		esbuildOptions: {
-			// Node.js global to browser globalThis
-			define: {
-				global: "globalThis",
-			},
-			plugins: [
-				NodeGlobalsPolyfillPlugin({
-					process: true,
-					buffer: true,
-				}),
-				NodeModulesPolyfillPlugin(),
-			],
-		},
-	},
-	build: {
-		sourcemap: "inline", // helpful for debugging, maybe remove in production
-		rollupOptions: {
-			plugins: [rollupNodePolyFill()],
-		},
-	},
-	define: {},
+    plugins: [
+        sveltekit(),
+        SvelteKitPWA(nostroidPWA),
+        NodeGlobalsPolyfillPlugin({
+            process: true,
+            buffer: true
+        }),
+        NodeModulesPolyfillPlugin()
+    ],
+    ssr: {
+        noExternal: ['nostr-tools']
+    },
+    resolve: {
+        alias: {
+            process: 'process/browser',
+            stream: 'rollup-plugin-node-polyfills/polyfills/stream',
+            util: 'rollup-plugin-node-polyfills/polyfills/util',
+            // buffer: 'rollup-plugin-node-polyfills/polyfills/buffer-es6'
+        }
+    },
+    optimizeDeps: {
+        include: [
+            'nostr-tools > create-hash',
+            'workbox-precaching',
+            'workbox-routing',
+            'workbox-window'
+        ],
+        esbuildOptions: {
+            // Node.js global to browser globalThis
+            define: {
+                global: 'globalThis'
+            },
+            plugins: [
+                NodeGlobalsPolyfillPlugin({
+                    process: true,
+                    buffer: true
+                }),
+                NodeModulesPolyfillPlugin()
+            ]
+        }
+    },
+    build: {
+        sourcemap: 'inline', // helpful for debugging, maybe remove in production
+        rollupOptions: {
+            plugins: [rollupNodePolyFill()]
+        }
+    },
+    define: {}
 });
