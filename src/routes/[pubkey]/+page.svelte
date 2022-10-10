f11633ecb78b275ad2dd5fe45ab5ffb1383604dc
Louis Huddleston <louishuddleston@gmail.com>
2022-10-10 17:52:37 +0100

    fix profile page

--- src/routes/[pubkey]/+page.svelte
+++ src/routes/[pubkey]/+page.svelte
@@ -25,7 +25,7 @@
 <li><strong>Name: </strong>{profile?.name}</li>
 <li><strong>Public Key: </strong>{profile?.pubkey}</li>
 <li><strong>Private Key: </strong> {#if profile?.privkey}yes{:else}no{/if}</li>
-<li><strong>Avatar: </strong>{#if profile?.avatar}<img src={profile?.avatar} alt='The Avatar'>{:else}no{/if}</li>
+<li><strong>Avatar: </strong>{#if profile?.avatar}<img class="avatar-img" src={profile?.avatar} alt='The Avatar'>{:else}no{/if}</li>
 </ul>
 
 {#if $events instanceof Array }
@@ -33,3 +33,12 @@
   <Event {event} oddRow={i % 2 === 0} />
 {/each}
 {/if}
+
+<style>
+	ul {
+		word-wrap: break-word;
+	}
+	.avatar-img {
+		width: 100%
+	}
+</style>
