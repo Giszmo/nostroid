acb64a47a800ebbb28b571282598e9386a79d31a
Louis Huddleston <louishuddleston@gmail.com>
2022-10-21 15:10:20 +0100

    create avatar image component

---
+++ src/components/AvatarImage.svelte
@@ -0,0 +1,21 @@
+<script lang='ts'>
+  import type { IProfile } from "../db"
+  export let profile: IProfile;
+
+  const robo = `https://robohash.org/${profile?.pubkey}.png`
+
+  $: avatar = profile?.avatar || robo
+</script>
+
+<img on:error={() => avatar = robo} src={avatar} alt="user's avatar">
+
+<style>
+  img {
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    object-fit: cover;
+    float: left;
+    background-color: white;
+  }
+</style>
