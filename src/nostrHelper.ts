b64775de354b20cb87d3e6a06a6ccfad2863be02
Louis Huddleston <louishuddleston@gmail.com>
2022-11-11 15:54:51 +0000

    add getDegreesForPubkey helper

--- src/nostrHelper.ts
+++ src/nostrHelper.ts
@@ -1,9 +1,10 @@
-import { db } from './db';
+import { db, type IProfile } from './db';
 import type { IEvent } from './db';
 // import { getPublicKey } from 'nostr-tools'
 // import {getEventHash, signEvent } from 'nostr-tools/event.js'
 import { getPublicKey } from './lib/nostr-tools';
 import { getEventHash, signEvent } from './lib/nostr-tools/event.js';
+import { chunks, filterMap, filterTF, forEach } from './utils/array';
 
 export const sendPersistEvent = async (kind, tags, content, privkey) => {
 	let pubkey = getPublicKey(privkey);
@@ -23,4 +24,50 @@ export const sendPersistEvent = async (kind, tags, content, privkey) => {
 	e.id = event.id;
 	e.sig = await signEvent(event, privkey);
 	db.events.add(e);
+};
+
+export const getDegreesForPubkey = async (pubkey: string, profiles: IProfile[]) => {
+	const profileFollows = new Map(
+		(await db.events.where('kind').equals(3).toArray()).map((it) => [
+			it.pubkey,
+			it.tags.filter((it) => it.startsWith('p»')).map((it) => it.split('»', 3)[1])
+		])
+	);
+
+	// n-th degree follows' pubkeys
+	const follows: Array<Set<string>> = [];
+	// 0-th
+	follows[0] = new Set([pubkey]);
+
+	// n-th
+	let all = new Set<string>();
+	for (let i = 0; i < 10; i++) {
+		all = new Set(follows.flatMap((it) => [...it]));
+		if (all.size > 10000) {
+			console.log(`Exploring ${all.size} follows to the ${i}-th degree ...`);
+			break;
+		}
+		// 1st
+		const fArray = Array.from(follows[i])
+			.flatMap((it) => profileFollows.get(it)!)
+			.filter((it) => !all.has(it));
+		const newSet = new Set(fArray);
+		if (newSet.size == 0) {
+			console.log(
+				`Exploring ${all.size} follows to the ${i}-th degree. No more follows found in the ${
+					i + 1
+				}-th degree ...`
+			);
+			break;
+		}
+		follows[i + 1] = newSet;
+	}
+
+	profiles = profiles.map((profile) => {
+		const deg = follows.findIndex((it) => it.has(profile.pubkey));
+		profile.degree = deg > -1 ? deg : 100;
+		return profile;
+	});
+
+	return profiles;
 };
